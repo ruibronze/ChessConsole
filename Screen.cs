c51bd52f7b441c2ed18030f7404ac550a3fa3fbe
Rui Bronze <ruibronze@gmail.com>
2021-05-19 18:00:49 -0300

    Board, BoardException, Piece, Position - scripts created

---
+++ Screen.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using board;
+using chess;
+
+namespace xadrez_console
+{
+    class Screen
+    {
+
+        public static void printMatch(ChessMatch match)
+        {
+            printMatch(match.board);
+            Console.WriteLine();
+            printCapturedPieces(match); 
+
+            Console.WriteLine();
+            Console.WriteLine("Shift: " + match.shift);
+            if (!match.over)
+            {
+                Console.WriteLine("Waiting a move: " + match.currentPlayer);
+                if (match.check)
+                {
+                    Console.WriteLine("  !! CHECK !!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("  !! CHECKMATE !!");
+                Console.WriteLine("Winner: " + match.currentPlayer);
+            }
+        }
+
+        public static void printCapturedPieces(ChessMatch match)
+        {
+            Console.WriteLine("Captured Pieces:");
+            Console.Write("Whites: ");
+           printCollection(match.capturedPieces(Color.White));
+            Console.WriteLine();
+            Console.Write("Pretas: ");
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            printCollection(match.capturedPieces(Color.Black));
+            Console.ForegroundColor = aux;
+            Console.WriteLine();
+        }
+
+        public static void printCollection(HashSet<Piece> collection)
+        {
+            Console.Write("[");
+            foreach (Piece x in collection)
+            {
+                Console.Write(x + " ");
+            }
+            Console.Write("]");
+        }
+
+        public static void printBoard(Board brd)
+        {
+
+            for (int i = 0; i < brd.lines; i++)
+            {
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < brd.columns; j++)
+                {
+                    printPiece(brd.piece(i, j));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void printBoard(Board brd, bool[,] possiblePositions)
+        {
+
+            ConsoleColor originalBackground = Console.BackgroundColor;
+            ConsoleColor otherBackground = ConsoleColor.DarkGray;
+
+            for (int i = 0; i < brd.lines; i++)
+            {
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < brd.columns; j++)
+                {
+                    if (possiblePositions[i, j])
+                    {
+                        Console.BackgroundColor = otherBackground;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = originalBackground;
+                    }
+                    imprimirPeca(brd.piece(i, j));
+                    Console.BackgroundColor = originalBackground;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+            Console.BackgroundColor = originalBackground;
+        }
+
+        public static ChessPosition readChessPosition()
+        {
+            string s = Console.ReadLine();
+            char column = s[0];
+            int line = int.Parse(s[1] + "");
+            return new ChessPosition(column, line);
+        }
+
+        public static void printPiece(Piece piece)
+        {
+
+            if (piece == null)
+            {
+                Console.Write("- ");
+            }
+            else
+            {
+                if (piece.color == Color.White)
+                {
+                    Console.Write(piece);
+                }
+                else
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(piece);
+                    Console.ForegroundColor = aux;
+                }
+                Console.Write(" ");
+            }
+        }
+
+    }
+}
---
+++ board/Board.cs
@@ -0,0 +1,72 @@
+﻿namespace board
+{
+    class Board
+    {
+
+        public int lines { get; set; }
+        public int columns { get; set; }
+        private Piece[,] pieces;
+
+        public Board(int lines, int columns)
+        {
+            this.lines = lines;
+            this.columns = columns;
+            pieces = new Piece[lines, columns];
+        }
+
+        public Piece Piece(int line, int column)
+        {
+            return pieces[line, column];
+        }
+
+        public Piece Piece(Position pos)
+        {
+            return pieces[pos.line, pos.column];
+        }
+
+        public bool anyPiece(Position pos)
+        {
+            validatePosition(pos);
+            return piece(pos) != null;
+        }
+
+        public void setPiece(Piece p, Position pos)
+        {
+            if (anyPiece(pos))
+            {
+                throw new BoardException("Já existe uma peça nessa posição!");
+            }
+            pieces[pos.line, pos.column] = p;
+            p.position = pos;
+        }
+
+        public Piece removePiece(Position pos)
+        {
+            if (piece(pos) == null)
+            {
+                return null;
+            }
+            Piece aux = piece(pos);
+            aux.position = null;
+            pieces[pos.line, pos.column] = null;
+            return aux;
+        }
+
+        public bool validPosition(Position pos)
+        {
+            if (pos.line < 0 || pos.line >= lines || pos.column < 0 || pos.column >= columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validatePosition(Position pos)
+        {
+            if (!validPosition(pos))
+            {
+                throw new BoardException("Posição inválida!");
+            }
+        }
+    }
+}
